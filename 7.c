4b60cd3c210515833ea78e1c8128fa980a61de23
adventus-glitch <kdlaagaman@gmail.com>
2025-01-27 23:19:02 +0545

    Program to calculate simple interets

--- 7.c
+++ 7.c
@@ -5,6 +5,6 @@ int main()
     printf("enter principal,rate and time respectively");
     scanf("%f%f%f",&p,&t,&r);
     i=(p*t*r)/100;
-    printf("the simple interest for the entered principal,rate and time is %f",i);//this is to calculate si
+    printf("the simple interest for the entered principal,rate and time is %f",i);
     return 0;
 }
