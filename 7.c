42fe9f6902d8c2038d77fdd3bfbafcd59e3aa8ce
adventus-glitch <kdlaagaman@gmail.com>
2025-01-27 23:24:21 +0545

    Program to calculate simple interest

---
+++ 7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float p,t,r,i;
+    printf("enter principal,rate and time respectively");
+    scanf("%f%f%f",&p,&t,&r);
+    i=(p*t*r)/100;
+    printf("the simple interest for the entered principal,rate and time is %f",i);
+    return 0;
+}
